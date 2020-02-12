458815069c81ed4d692e1d456a9191533891fdae
yourleige@gmail.com <Fang201314>
2020-02-12 14:15:37 +0800

    1、屏蔽elk，改成传统日志输出。 2、增加表单重复提交自定义注解。

--- pigx-upms/pigx-upms-biz/src/test/java/com/pig4cloud/pigx/admin/PigxAdminApplicationTest.java
+++ pigx-upms/pigx-upms-biz/src/test/java/com/pig4cloud/pigx/admin/PigxAdminApplicationTest.java
@@ -22,6 +22,13 @@ import org.jasypt.encryption.StringEncryptor;
 import org.junit.Test;
 import org.springframework.core.env.StandardEnvironment;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * @author lengleng
  * @date 2018/10/7
@@ -29,6 +36,9 @@ import org.springframework.core.env.StandardEnvironment;
  * 加解密单元测试
  */
 public class PigxAdminApplicationTest {
+
+	static Map<Integer,Integer>  map=new HashMap<>(2);
+
 	@Test
 	public void testJasypt() {
 		// 对应application-dev.yml 中配置的根密码
@@ -40,5 +50,34 @@ public class PigxAdminApplicationTest {
 
 		//解密方法
 		System.out.println(stringEncryptor.decrypt("ltJPpR50wT0oIY9kfOe1Iw==="));
+	}
+
+
+	@Test
+	public void testHashMap() throws InterruptedException{
+		final AtomicInteger at=new AtomicInteger(0);
+		final CountDownLatch countDownLatch=new CountDownLatch(100);//并发计数器
+		for (int i=0;i<100;i++){
+           new Thread(new Runnable() {
+			   @Override
+			   public void run() {
+				  while (at.get()<1000000){
+                     //向map中添加元素
+					  map.put(at.get(),at.get());
+					  at.incrementAndGet();//对int加1
+				  }
+				   countDownLatch.countDown();
+			   }
+		   }
+		   ).start();
+		}
+        //阻塞当前主线程
+		countDownLatch.await();
+		Set<Integer> keySet=map.keySet();
+		Iterator<Integer> iterator=keySet.iterator();
+		while (iterator.hasNext()){
+			Integer key= iterator.next();
+			System.out.println("key:"+key+"- -value:"+map.get(key));
+		}
 	}
 }
